2ecb740c4b29557b393c5d705da8e5de150675eb
Nguyen Le Ly Bang <bangjdev@gmail.com>
2016-05-30 22:57:12 +0700

    Add new method: sayAPart()

--- TTS.java
+++ TTS.java
@@ -1,4 +1,10 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 public class TTS {
 	String sourceFile;
@@ -33,12 +39,50 @@ public class TTS {
 	public void setSpeed(int speed) {
 		this.speed = speed;
 	}
+
 	public void say() throws IOException, InterruptedException {
 		String command = "espeak -s " + getSpeed() + " -v " + getVoice() + " -f " + getSourceFile();
 		Process pro = Runtime.getRuntime().exec(command);
-		
+
 	}
-	public void sayAPart(int head, int ass) { // =)) LOLOLOL
-		
+
+	/*
+	 * This method is used to say a part in file, from line head to ass
+	 */
+	public void sayAPart(int head, int ass, boolean wbw) throws IOException, InterruptedException {
+		// Load file
+		String line[] = new String[100000];
+		int n = 0;
+		FileInputStream fis = new FileInputStream(new File(getSourceFile()));
+		BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+		String l = br.readLine();
+		while (l != null) {
+			System.out.println(l);
+			Thread.sleep(1000);
+			n++;
+			line[n] = l;
+			l = br.readLine();
+		}
+		fis.close();
+		br.close();
+
+		// Say
+		String temp = getSourceFile();
+		setSourceFile("temporary_text_file.txt");
+		PrintWriter pw = new PrintWriter(new File(getSourceFile()));
+		for (int i = head; i <= min(n, ass); i++) {
+			pw.println(line[i]);
+		}
+		pw.flush();
+		pw.close();
+		say();
+		setSourceFile(temp);
+	}
+
+	private int min(int n, int ass) {
+		if (n < ass)
+			return n;
+		return ass;
 	}
 }
+
